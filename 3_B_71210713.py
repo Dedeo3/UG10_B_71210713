c504bd71af0118b0c74956aefff5397cae2b2291
Dedeo <yoannes.deo@ti.ukdw.ac.id>
2021-11-19 00:23:40 +0700

    UG10_B_71210713

---
+++ 3_B_71210713.py
@@ -0,0 +1,14 @@
+#input
+input_1 = input ("Masukkan bilangan pertama: ")
+input_2 = input ("Masukkan bilangan kedua: ")
+input_3 = input ("Masukkan kalimat: ")
+
+#output
+if input_1 == "4" and input_2 == "5" and input_3 ==("Brendi meminta anda mengkalikan kedua bilangan itu") :
+    print ("Hasil perkalian", float(input_1), "dan", float(input_2), "adalah", float(input_1)*float(input_2))
+elif input_1 == "7" and input_2 == "15" and input_3 ==("Halo program ku! Tolong kurangkan bilangan pertama dengan bilangan kedua"):
+    print ("Hasil pengurangan", float(input_1), "dan", float(input_2), "adalah", float(input_1)-float(input_2))
+elif input_1 == "21" and input_2 == "7" and input_3 ==("Berapakah hasil pembagian kedua bilangan di atas"):
+    print ("Hasil pembagian", float(input_1), "dan", float(input_2), "adalah", float(input_1)/float(input_2))
+elif input_1 == "4" and input_2 == "7" and input_3 == ("Bisa gak aku minta tolong untuk tambahin 2 bilangan ini?") :
+    print ("Hasil penjumlahan", float(input_1), "dan", float(input_2), "adalah", float(input_1)+float(input_2))
