cbdc7684034bc7fdeefeeb9fca3a7ca64f2ac910
Dedeo <yoannes.deo@ti.ukdw.ac.id>
2021-11-19 00:21:27 +0700

    UG10_B_71210713

---
+++ 1_B_71210713.py
@@ -0,0 +1,9 @@
+nilai = input("Masukkan nilai huruf Prakom anda : ")
+if nilai == "A":
+    print("="*50)
+    print("Rentan nilai Prakom anda adalah 85-100")
+elif nilai == "B-":
+    print("="*50)
+    print("Rentan nilai Prakom anda adalah 65-69")
+elif nilai == "C++":
+    print("Inputan anda salah atau nilai huruf tidak ada pada standar Nilai")
